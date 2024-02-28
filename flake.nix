1fdb5019ec2414f0f25bb4b5356f86fab29c264d
Joshua Tag Howard <jtho264@uky.edu>
2024-02-28 16:54:41 -0500

    Revert "Switch to unstable"
    This reverts commit 4f0a935dae805742db433d78c37e890b80952d8f.

--- flake.nix
+++ flake.nix
@@ -4,20 +4,15 @@
   inputs = {
     # NixOS official package source, using the nixos-23.11 branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
-    # NixOS unstable package source
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-    # Home Manager
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs-unstable";
-
-    # nixd
+    home-manager.url = "github:nix-community/home-manager/release-23.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nixd.url = "github:nix-community/nixd";
   };
 
-  outputs = { self, nixpkgs-unstable, home-manager, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
     # Please replace my-nixos with your hostname
-    nixosConfigurations.aurora-desktop = nixpkgs-unstable.lib.nixosSystem {
+    nixosConfigurations.aurora-desktop = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { inherit inputs; };
       modules = [
@@ -25,6 +20,16 @@
         # so the old configuration file still takes effect
         ./configuration.nix
         ./mirrors.nix
+        {
+          nixpkgs.overlays = [
+            (final: prev: {
+              unstable = import inputs.nixpkgs-unstable {
+                system = "x86_64-linux";
+                config.allowUnfree = true;
+              };
+            })
+          ];
+        }
         home-manager.nixosModules.home-manager
         {
           home-manager.useGlobalPkgs = true;
