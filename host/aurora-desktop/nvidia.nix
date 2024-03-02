22f63d7a23ac9005f8668f32010f8f9fa7418458
Joshua Tag Howard <jtho264@uky.edu>
2024-03-01 19:21:00 -0500

    30 current  2024-03-01 19:20:44  24.05.20240229.1536926   6.6.18                          *

--- host/aurora-desktop/nvidia.nix
+++ host/aurora-desktop/nvidia.nix
@@ -22,14 +22,16 @@
     # supported GPUs is at:
     # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
     # Only available from driver 515.43.04+
-    # Currently alpha-quality/buggy, so false is currently the recommended setting.
-    open = true;
+    open = false;
 
     # Enable the Nvidia settings menu,
     # accessible via `nvidia-settings`.
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.production;
   };
+
+  boot.initrd.kernelModules = ["nvidia"];
+  boot.extraModulePackages = [config.boot.kernelPackages.nvidia_x11];
 }
